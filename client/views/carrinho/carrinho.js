4b988bf724deaad22e9f2bfa11f88f78ffeb91d5
Renato Ramiro <renato.ipj@gmail.com>
2014-09-12 16:48:44 -0300

    Adicionado a opção de esvaziar carrinho

--- client/views/carrinho/carrinho.js
+++ client/views/carrinho/carrinho.js
@@ -1,3 +1,11 @@
 Template.carrinho.cart = function(){
   return Carts.findOne({_id: Session.get('carts')});
-}
+};
+
+Template.carrinho.events({
+	'click #clearCart': function(e, t){
+		e.preventDefault();
+		var cart = Session.get('carts');
+		Carts.update({_id: cart}, {$set: { products: []}});
+	}
+});
