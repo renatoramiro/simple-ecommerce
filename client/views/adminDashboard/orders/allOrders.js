6cdd9feb3e0701e662f647c10661bbaa6bb0d03d
Renato Ramiro <renato.ipj@gmail.com>
2014-10-13 22:11:33 -0300

    Adicionado total da compra em admin pedidos e em minhas compras

--- client/views/adminDashboard/orders/allOrders.js
+++ client/views/adminDashboard/orders/allOrders.js
@@ -34,5 +34,13 @@ Template.allOrders.helpers({
 
   limite: function () {
   	return Session.get('limit');
+  },
+
+  total: function () {
+    var total = 0.0;
+    _.map(this.products, function (product) {
+      total += product.price * product.quantity;
+    });
+    return total;
   }
 });
--- client/views/purchases/purchases.js
+++ client/views/purchases/purchases.js
@@ -2,6 +2,14 @@ Template.purchases.helpers({
 	purchases: function () {
 		return Orders.find({userId: Meteor.userId()}, {sort: {createdAt: -1}});
 	},
+
+	total: function () {
+		var total = 0.0;
+		_.map(this.products, function (product) {
+			total += product.price * product.quantity;
+		});
+		return total;
+	}
 });
 
 Template.showPurchase.helpers({
