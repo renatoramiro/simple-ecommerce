609be771406a433be10a029b8fcc1b128960814e
Renato Ramiro <renato.ipj@gmail.com>
2014-10-18 22:38:58 -0300

    Melhorado reutilização de codigo e algumas outras melhorias nas views

--- client/helpers/limitOrders.js
+++ client/helpers/limitOrders.js
@@ -5,3 +5,11 @@ UI.registerHelper('limitPagination', function (limit) {
     	return true;
     }
 });
+
+UI.registerHelper('somaProdutos', function (products) {
+	var total = 0.0;
+    _.map(this.products, function (product) {
+      total += product.price * product.quantity;
+    });
+    return accounting.formatMoney(total, "R$ ", 2, ".", ",");
+})
--- client/views/adminDashboard/orders/showAdminOrder.js
+++ client/views/adminDashboard/orders/showAdminOrder.js
@@ -1,9 +1,5 @@
 Template.showAdminOrder.helpers({
-	total: function () {
-		var total = 0.0;
-    _.map(this.products, function (product) {
-      total += product.price * product.quantity;
-    });
-    return total;
+	linkUser: function () {
+		return Router.routes['showUser'].url({_id: this.userId});
 	}
 })
